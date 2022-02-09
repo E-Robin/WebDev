df2cfccecee0a88431a1fed3577df1acb0d58da3
E-Robin <grobin578@gmail.com>
2022-02-09 10:47:49 +0530

    css updated

--- script.js
+++ script.js
@@ -3,14 +3,12 @@
 
 
 
-
-
-
-
 function update(){
     console.log("hi rob")
+    var output=document.getElementById("updates");
     
-document.getElementById("robi").innerHTML=document.getElementById("updates").value;
+document.getElementById("robi").innerText=output.value;
+document.getElementById("rob").innerText=output.value;
 }
 
 
@@ -18,13 +16,26 @@ document.getElementById("robi").innerHTML=document.getElementById("updates").val
 
 function update1(){
     console.log("hi rob")
-    document.getElementById("num").innerHTML=document.getElementById("updateNumber").value;
+    var output=document.getElementById("updateNumber");
+    
+document.getElementById("num").innerText=output.value;
+document.getElementById("num1").innerText=output.value;
+
+    // document.getElementById("num").innerHTML=document.getElementById("updateNumber").value;
 }
 
 function update3(){
-    console.log("hi rob")
-    document.getElementById("email").innerHTML=document.getElementById("updateEmail").value;
+    console.log("hi email")
+    var output=document.getElementById("updateEmail");
+    
+document.getElementById("email").innerText=output.value;
+document.getElementById("email1").innerText=output.value;
 }
+    // document.getElementById("email").innerHTML=document.getElementById("updateEmail").value;
+
+
+
+
 
 
 
